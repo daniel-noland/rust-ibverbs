068dfd06320865ec71ec2fff1b2acea1096cc8af
Daniel Noland <daniel@bestateless.com>
2022-02-06 19:20:08 -0700

    ci hacking

--- scripts/prepare-ci.sh
+++ scripts/prepare-ci.sh
@@ -36,8 +36,9 @@ build_and_install_soft_roce_kernel_module() {
   (
     set -x;
     pushd /tmp/kernel-source;
-    apt-get source --yes "linux-image-unsigned-$(uname --kernel-release)";
-    pushd "/tmp/kernel-source/linux-azure-$(uname --kernel-release)";
+    apt-get source --yes "linux-image-unsigned-$(uname --kernel-release)" >/dev/null;
+    ls;
+    pushd "/tmp/kernel-source/linux-azure-5.11.0";
 #    apt-get source --yes "linux-image-unsigned-5.4.0-1010-azure";
 #    pushd "/tmp/kernel-source/linux-azure-5.4.0";
     apt-cache search linux-buildinfo | grep azure;
