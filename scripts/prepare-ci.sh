75f454e67253b76279d9fc5f2ea28804befb7163
Daniel Noland <daniel@bestateless.com>
2022-02-06 19:06:51 -0700

    ci hacking

---
+++ scripts/prepare-ci.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -euxETo pipefail
+
+# Activate deb-src archives
+sed --in-place 's/^# deb-src/deb-src/' /etc/apt/sources.list
+
+apt-get update
+
+apt-get install --yes --no-install-recommends \
+  dpkg-dev \
+;
+
+# Install kernel build dependencies
+install_kernel_build_dependencies() {
+
+  apt-get build-dep --yes --no-install-recommends \
+   linux-azure \
+  ;
+
+  # build-dep install isn't very reliable.  It misses these packages.
+  apt-get install --yes --no-install-recommends \
+    bc \
+    bison \
+    flex \
+    libelf-dev \
+    libssl-dev \
+    ncurses-dev \
+  ;
+}
+
+build_and_install_soft_roce_kernel_module() {
+  uname -a;
+  uname --kernel-release;
+  mkdir --parent /run/kernel-source
+  (
+    set -x;
+    pushd /run/kernel-source2;
+    apt-get source --yes "linux-image-unsigned-$(uname --kernel-release)-azure";
+    pushd "/run/kernel-source/linux-azure-$(uname --kernel-release)";
+#    apt-get source --yes "linux-image-unsigned-5.4.0-1010-azure";
+#    pushd "/run/kernel-source/linux-azure-5.4.0";
+    apt-cache search linux-buildinfo | grep azure;
+    apt-get install --yes linux-buildinfo-"$(uname --kernel-release)-azure"
+#    apt-get install --yes "linux-buildinfo-5.4.0-1010-azure";
+    cp "/usr/lib/linux/$(uname --kernel-release)-1010-azure/config" ./.config;
+#    cp "/usr/lib/linux/5.4.0-1010-azure/config" ./.config;
+    make olddefconfig;
+#    cp "/usr/src/linux-headers-5.11.0-1028-azure/Module.symvers" ./;
+    cp "/usr/src/linux-headers-$(uname --kernel-release)-azure/Module.symvers" ./;
+    sed --in-place 's/# CONFIG_RDMA_RXE is not set/CONFIG_RDMA_RXE=m/' ./.config;
+    make --jobs="$(nproc)" prepare;
+    make --jobs="$(nproc)" modules_prepare;
+#    make --jobs="$(nproc)" drivers/infiniband/core/ib_core.ko;
+    make --jobs="$(nproc)" drivers/infiniband/sw/rxe/rdma_rxe.ko;
+    modprobe ib_core
+    cp ./drivers/infiniband/core/ib_core.ko /lib/modules/;
+    insmod ./drivers/infiniband/sw/rxe/rdma_rxe.ko;
+#    cp "/usr/src/linux-headers-$(uname --kernel-release)-1010-azure/Module.symvers" ./;
+  )
+}
+
+# Install rdma libs
+install_rdma_libs() {
+  apt-get install --yes --no-install-recommends \
+    libibverbs-dev \
+    libnl-3-dev \
+    libnl-route-3-dev \
+    rdma-core \
+  ;
+}
+
+install_kernel_build_dependencies
+build_and_install_soft_roce_kernel_module
