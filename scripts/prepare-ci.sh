f18cdb6de3d906da39c995cf1d5844329e83d2e0
Daniel Noland <daniel@bestateless.com>
2022-02-06 21:37:30 -0700

    ci hacking

--- scripts/prepare-ci.sh
+++ scripts/prepare-ci.sh
@@ -33,40 +33,37 @@ build_and_install_soft_roce_kernel_module() {
   rm --force --recursive /tmp/kernel-source # TODO: ditch this line, it's crazy outside your test env
   mkdir /tmp/kernel-source
   ls -lahR /boot;
-  apt-get install --yes linux-modules-extra-azure;
-  depmod --all;
-  modprobe rdma_rxe;
-#
-#  (
-#    set -x;
-#    declare kernel_release
-##    kernel_release="$(uname --kernel-release)"
-#    kernel_release="5.11.0-1028-azure"
-#    declare -r kernel_release
-#    declare -r kernel_version="${kernel_release%%-*}"
-#    declare -r kernel_maj_min="${kernel_version%.*}"
-#
-#    pushd /tmp/kernel-source;
-#    apt-get source --yes "linux-image-unsigned-${kernel_release}" >/dev/null;
-#    ls;
-#    pushd "/tmp/kernel-source/linux-azure-${kernel_maj_min}-${kernel_version}";
-#    apt-get source --yes "linux-image-unsigned-${kernel_release}";
-#    cp "/boot/config-${kernel_release}" ./.config;
-#    cp "/usr/src/linux-headers-${kernel_release}/Module.symvers" ./;
-#    make olddefconfig;
-#    sed --in-place 's/# CONFIG_RDMA_RXE is not set/CONFIG_RDMA_RXE=m/' ./.config;
-#    make --jobs="$(nproc)" prepare;
-#    make --jobs="$(nproc)" modules_prepare;
-#    make --jobs="$(nproc)" scripts/mod;
+
+  (
+    set -x;
+    declare kernel_release
+#    kernel_release="$(uname --kernel-release)"
+    kernel_release="5.11.0-1028-azure"
+    declare -r kernel_release
+    declare -r kernel_version="${kernel_release%%-*}"
+    declare -r kernel_maj_min="${kernel_version%.*}"
+    pushd /tmp/kernel-source;
+    apt-get source --yes "linux-image-unsigned-${kernel_release}" >/dev/null;
+    pushd "/tmp/kernel-source/linux-azure-${kernel_maj_min}-${kernel_version}";
+    cp "/boot/config-${kernel_release}" ./.config;
+    cp "/usr/src/linux-headers-${kernel_release}/Module.symvers" ./;
+    sed --in-place 's/# CONFIG_RDMA_RXE is not set/CONFIG_RDMA_RXE=m/' ./.config;
+    make olddefconfig;
+    make --jobs="$(nproc)" prepare;
+    make --jobs="$(nproc)" modules_prepare;
 #    make --jobs="$(nproc)";
 #    make --jobs="$(nproc)" modules;
 #    make --jobs="$(nproc)" modules_install;
-##    modprobe ib_core
-##    mkdir --parent "/lib/modules/${kernel_release}/kernel/drivers/infiniband/sw/rxe";
-##    cp ./drivers/infiniband/sw/rxe/rdma_rxe.ko "/lib/modules/${kernel_release}/kernel/drivers/infiniband/sw/rxe";
-#    depmod --all;
-#    modprobe rdma_rxe;
-#  )
+#    make -C "/lib/modules/${kernel_release}/build" M="$(pwd)/drivers/infiniband/sw/rxe/rdma_rxe.ko" modules
+    make M="drivers/infiniband" modules_install || true
+    insmod ./drivers/infiniband/sw/rxe/rdma_rxe.ko
+
+#    modprobe ib_core
+#    mkdir --parent "/lib/modules/${kernel_release}/kernel/drivers/infiniband/sw/rxe";
+#    cp ./drivers/infiniband/sw/rxe/rdma_rxe.ko "/lib/modules/${kernel_release}/kernel/drivers/infiniband/sw/rxe";
+    depmod --all;
+    modprobe rdma_rxe;
+  )
 }
 
 # Install rdma libs
